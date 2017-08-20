e1dc369ba7379733a587f6b35c30ba66883e3bff
Ryan Burbank <ryanburbank@Ryans-MacBook-Pro.local>
2017-08-20 10:46:57 -0400

    Initial Commit

---
+++ Project4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Ryan Burbank on 8/20/17.
+//  Copyright © 2017 Ryan Burbank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
